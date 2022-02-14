413d312216c7482b20dd793e72e9cf46ce876759
Carter Allen <bcarter.allen1@gmail.com>
2022-02-14 17:48:27 -0500

    added KO Type to benchling df

--- benchling_data_clean.py
+++ benchling_data_clean.py
@@ -8,6 +8,13 @@ from tkinter import Tk
 from tkinter.filedialog import askdirectory
 from tkinter.filedialog import askopenfilename
 
+MIN_R_SQUARED = 0.85
+MIN_HOMOZYGOUS_KO_SCORE = 85
+MAX_HETEROZYGOUS_KO_SCORE = 70
+MIN_HETEROZYGOUS_KO_SCORE = 35
+
+ZYGOUSITY_LABEL = "ko_type"
+
 # Running a batch analysis
 def benchling_data_clean():
     """
@@ -17,27 +24,35 @@ def benchling_data_clean():
     4. Filter results based on logic.
     5. Generate a .csv
     """
-    
+
+    #Prevent Tkinter Gui Screen from popping up
     Tk().withdraw()
+
     print("Choose Results Excel File: ")
     excel_path = askopenfilename()
     print("Choose Destination folder for benchling csv")
     output_dir = askdirectory()
+
+    #Store Results timestamp
     results_timestamp = str(excel_path).split('.')[2]
 
-
+    #Process Data
     raw_results_df = pd.read_excel(excel_path, index_col=0)
     raw_results_df['Position'] = raw_results_df['sample_name'].map(lambda x: int(re.findall('_(\d+)-',str(x))[0]))
     raw_results_df['Well'] = raw_results_df['Position'].map(lambda x: convertNumericWellPosToA1Style(int(x),8,12,"col",True))
     no_na_df = raw_results_df.dropna(subset=["ice"])
-    quality_df = no_na_df[(no_na_df["r_squared"] >= 0.85)]
+    quality_df = no_na_df[(no_na_df["r_squared"] >= MIN_R_SQUARED)]
+    #Filter results to 
     homo_hetero_df = quality_df[
-        (quality_df["ko_score"] >= 85) |
-        (((quality_df["ko_score"] >= 35)  & (quality_df["ko_score"] <= 70)))
+        (quality_df["ko_score"] >= MIN_HOMOZYGOUS_KO_SCORE) |
+        (((quality_df["ko_score"] >= MIN_HETEROZYGOUS_KO_SCORE)  & (quality_df["ko_score"] <= MAX_HETEROZYGOUS_KO_SCORE)))
     ]
 
+    homo_hetero_df[ZYGOUSITY_LABEL] = homo_hetero_df.apply (lambda row: check_zygosity(row), axis=1)
+    clean_df = homo_hetero_df[["sample_name","r_squared","ko_score","Guide Sequence","Well", ZYGOUSITY_LABEL]].copy()
+
     output_path = os.path.join(output_dir,"benchling_results_{}.csv".format(results_timestamp))
-    homo_hetero_df.to_csv(output_path, index=False)
+    clean_df.to_csv(output_path, index=False)
 
     print(output_dir)
     
@@ -78,6 +93,14 @@ def convertNumericWellPosToA1Style(numericPos, contRows, contCols, colOrRowMajor
             return 'ABCDEFGH'[(numericPos - 1) % contRows] + str(math.ceil(numericPos / contRows))
     else:
         raise ValueError("Container traversal value '" + colOrRowMajor + "' must be 'col' or 'row'.")
+
+def check_zygosity(row):
+    if row["ko_score"] >= MIN_HOMOZYGOUS_KO_SCORE:
+        return "Homozygous"
+    if row["ko_score"] <= MAX_HETEROZYGOUS_KO_SCORE and row["ko_score"] >= MIN_HETEROZYGOUS_KO_SCORE:
+        return "Heterozygous"
+    else:
+        return "NaN"
 
 if __name__ == '__main__':
     benchling_data_clean()
